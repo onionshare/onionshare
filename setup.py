27e269f6467bf6308c5fc6d04c2e7a280031c35e
Micah Lee <micah@micahflee.com>
2014-06-24 17:36:01 -0400

    removing windows code from setup.py, because building for windows uses pyinstaller now (#35)

--- setup.py
+++ setup.py
@@ -38,25 +38,6 @@ if platform.system() == 'Darwin':
     }
     args['setup_requires'] = 'py2app'
 
-elif platform.system() == 'Windows':
-    import py2exe
-    args['windows'] = [{'script':'setup/onionshare-launcher.py'}]
-    args['data_files'] = [
-        ('', ['LICENSE', 'README.md', 'version']),
-        ('onionshare', ['onionshare/index.html', 'onionshare/404.html', 'onionshare/strings.json']),
-        ('onionshare_gui', ['onionshare_gui/onionshare-icon.png']),
-        ('onionshare_gui/templates', glob('onionshare_gui/templates/*')),
-        ('onionshare_gui/static', glob('onionshare_gui/static/*'))
-    ]
-    args['options'] = {
-        'py2exe': {
-            'includes': ['sip', 'PyQt4', 'PyQt4.QtNetwork'],
-            'dll_excludes': ['MSVCP90.dll'],
-            'packages': ['jinja2', 'flask', 'stem'],
-            'skip_archive': True
-        }
-    }
-
 else:
     args['packages'] = ['onionshare', 'onionshare_gui']
     args['include_package_data'] = True
