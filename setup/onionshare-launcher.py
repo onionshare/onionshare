e8cc7c3a4a4d89e8c481a70d0c046e7e393fc980
Micah Lee <micah@micahflee.com>
2014-07-16 04:48:01 -0700

    fixed issue with pyinstaller packaging Qt4 on some OSX

--- setup/onionshare-launcher.py
+++ setup/onionshare-launcher.py
@@ -1,6 +1,6 @@
 # import stuff for pyinstaller to find
 import os, sys, subprocess, time, hashlib, platform, json, locale, socket, argparse, Queue, inspect, base64, random, functools, logging
-import PyQt4.QtCore, PyQt4.QtGui, PyQt4.QtWebKit
+from PyQt4 import QtCore, QtGui, QtWebKit
 import stem, stem.control, flask
 import onionshare, onionshare_gui
 
