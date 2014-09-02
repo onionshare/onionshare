a6e99fcc21178c46f52192766b15c5d27c33f0c9
Micah Lee <micah@micahflee.com>
2014-09-02 12:10:42 -0700

    added GPLv3 license to each source file

--- setup/onionshare-launcher.py
+++ setup/onionshare-launcher.py
@@ -1,3 +1,21 @@
+"""
+OnionShare | https://onionshare.org/
+
+Copyright (C) 2014 Micah Lee <micah@micahflee.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 from __future__ import division
 import os, sys, subprocess, time, hashlib, platform, json, locale, socket, argparse, Queue, inspect, base64, random, functools, logging, ctypes, hmac, shutil
 from itertools import izip
