5deb3f9e0f5b7cd81602f90f859c2ec0e4e41eec
Thomas Waldmann <tw@waldmann-edv.de>
2014-11-18 18:03:34 +0100

    pep8: fix indentation to be a multiple of four
    note: i used pycharm "reformat file", so there are other reformattings also

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -18,22 +18,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import tempfile
 
+
 class MockSubprocess():
-  def __init__(self):
-      self.last_call = None
+    def __init__(self):
+        self.last_call = None
 
-  def call(self, args):
-      self.last_call = args
+    def call(self, args):
+        self.last_call = args
 
-  def last_call_args(self):
-      return self.last_call
+    def last_call_args(self):
+        return self.last_call
+
 
 def write_tempfile(text):
     tempdir = tempfile.mkdtemp()
     path = tempdir + "/test-file.txt"
     with open(path, "w") as f:
-      f.write(text)
-      f.close()
+        f.write(text)
+        f.close()
     return path
 
 
