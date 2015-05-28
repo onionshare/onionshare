41671e58dfb1ebebf5e0fabd361ad35d72fdc73d
Freddy Martinez <freddymartine9@gmail.com>
2015-05-28 04:52:27 -0500

    fix for #188

--- build_deb.sh
+++ build_deb.sh
@@ -1,14 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+cd $DIR
 
 VERSION=`cat version`
 
 # clean up from last build
-rm -r deb_dist
+rm -r deb_dist >/dev/null 2>&1
 
 # build binary package
 python setup.py --command-packages=stdeb.command bdist_deb
 
-# install it
-echo ""
-echo "To install, run:"
-echo "sudo dpkg -i deb_dist/onionshare_$VERSION-1_all.deb"
+# return install instructions if onionshare builds properly
+if [$? -eq 0] then 
+	echo ""
+	echo "To install, run:"
+	echo "sudo dpkg -i deb_dist/onionshare_$VERSION-1_all.deb"
+else 
+	echo "OnionShare failed to build!" 
+fi
