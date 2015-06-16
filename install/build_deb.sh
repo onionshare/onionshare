d50601f5bd3c3312e89c6f9c457fc61697d936d4
Micah Lee <micah@micahflee.com>
2015-06-16 15:42:39 -0700

    fixed typo in build_deb.sh

--- install/build_deb.sh
+++ install/build_deb.sh
@@ -12,10 +12,10 @@ rm -r deb_dist >/dev/null 2>&1
 python setup.py --command-packages=stdeb.command bdist_deb
 
 # return install instructions if onionshare builds properly
-if [$? -eq 0] then 
-	echo ""
+echo ""
+if [[ $? -eq 0 ]]; then
 	echo "To install, run:"
 	echo "sudo dpkg -i deb_dist/onionshare_$VERSION-1_all.deb"
-else 
-	echo "OnionShare failed to build!" 
+else
+	echo "OnionShare failed to build!"
 fi
