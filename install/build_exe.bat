acd8afeb80ad9dc1f20e2cddf2dfe6749227ba3c
Micah Lee <micah@micahflee.com>
2016-09-05 02:54:25 -0700

    Update build_exe.bat and NSIS installer config to support cx_Freeze

--- install/build_exe.bat
+++ install/build_exe.bat
@@ -1,10 +1,12 @@
-REM use PyInstaller to builder a folder with onionshare.exe
-pyinstaller install\pyinstaller.spec -y
+REM build onionshare.exe, onionshare-gui.exe
+python setup.py build
 
-REM sign onionshare.exe
-signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 dist\onionshare\onionshare.exe
+REM sign onionshare.exe, onionshare-gui.exe
+signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 build\exe.win32-3.5\onionshare.exe
+signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 build\exe.win32-3.5\onionshare-gui.exe
 
 REM build an installer, dist\OnionShare_Setup.exe
+mkdir dist
 makensis.exe install\onionshare.nsi
 
 REM sign OnionShare_Setup.exe
