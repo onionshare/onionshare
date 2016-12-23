fd41eac48d1afd4a8399e05f393fecb4cccc099a
Micah Lee <micah@micahflee.com>
2016-12-22 16:19:54 -0800

    Added dev scripts for launching onionshare and onionshare-gui from the source code tree, without having to install it

--- onionshare/helpers.py
+++ onionshare/helpers.py
@@ -34,14 +34,17 @@ def get_resource_path(filename):
     systemwide, and whether regardless of platform
     """
     p = get_platform()
-    if p == 'Linux' and sys.argv and sys.argv[0].startswith(sys.prefix):
+
+    if getattr(sys, 'onionshare_dev_mode', False):
+        # Look for resources directory relative to python file
+        resources_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))), 'resources')
+
+    elif p == 'Linux' and sys.argv and sys.argv[0].startswith(sys.prefix):
         # OnionShare is installed systemwide in Linux
         resources_dir = os.path.join(sys.prefix, 'share/onionshare')
     elif getattr(sys, 'frozen', False): # Check if app is "frozen" with cx_Freeze
         # http://cx-freeze.readthedocs.io/en/latest/faq.html#using-data-files
         resources_dir = os.path.join(os.path.dirname(sys.executable), 'resources')
-    else:  # Look for resources directory relative to python file
-        resources_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))), 'resources')
 
     return os.path.join(resources_dir, filename)
 
