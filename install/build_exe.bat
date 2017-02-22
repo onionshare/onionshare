b33fe005110de8779b225838236622ec35e46153
Micah Lee <micah@micahflee.com>
2017-02-22 14:28:50 -0800

    Update Windows build script to work with PyInstaller, update readme about only having onionshare-gui.exe, and update PyInstaller spec to create an 'onionshare' folder instead of an 'onionshare-gui' one

--- install/build_exe.bat
+++ install/build_exe.bat
@@ -1,12 +1,14 @@
-REM build onionshare.exe, onionshare-gui.exe
+REM delete old build files
+rmdir /s /q build
+rmdir /s /q dist
+
+REM build onionshare-gui.exe
 pyinstaller install\pyinstaller.spec -y
 
-REM sign onionshare.exe, onionshare-gui.exe
-signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 build\exe.win32-3.5\onionshare.exe
-signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 build\exe.win32-3.5\onionshare-gui.exe
+REM sign onionshare-gui.exe
+signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 dist\onionshare\onionshare-gui.exe
 
 REM build an installer, dist\OnionShare_Setup.exe
-mkdir dist
 makensis.exe install\onionshare.nsi
 
 REM sign OnionShare_Setup.exe
