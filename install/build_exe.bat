6870cf36d32ba8079d7fdaa3038d7d10ceaa8b65
Micah Lee <micah@micahflee.com>
2017-04-08 17:29:00 -0700

    Wrote script to download pre-built tor binary for Windows

--- install/build_exe.bat
+++ install/build_exe.bat
@@ -1,9 +1,11 @@
-REM delete old build files
-rmdir /s /q build
+REM delete old dist files
 rmdir /s /q dist
 
 REM build onionshare-gui.exe
 pyinstaller install\pyinstaller.spec -y
+
+REM download tor
+python install\get-tor-windows.py
 
 REM sign onionshare-gui.exe
 signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 dist\onionshare\onionshare-gui.exe
