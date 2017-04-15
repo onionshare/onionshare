984d2b6084e9d5ff0b779bdf79170cca204b24de
Micah Lee <micah@micahflee.com>
2017-04-14 18:33:44 -0700

    Added support for bundled tor connection type in Windows, including using ports instead of socket files

--- install/build_exe.bat
+++ install/build_exe.bat
@@ -8,10 +8,10 @@ REM download tor
 python install\get-tor-windows.py
 
 REM sign onionshare-gui.exe
-signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 dist\onionshare\onionshare-gui.exe
+REM signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 dist\onionshare\onionshare-gui.exe
 
 REM build an installer, dist\OnionShare_Setup.exe
-makensis.exe install\onionshare.nsi
+REM makensis.exe install\onionshare.nsi
 
 REM sign OnionShare_Setup.exe
-signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 dist\OnionShare_Setup.exe
+REM signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 dist\OnionShare_Setup.exe
