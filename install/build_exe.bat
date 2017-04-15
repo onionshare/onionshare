5a18b33aa4ae0c04c81e9b97446e7e37384e01ad
Micah Lee <micah@micahflee.com>
2017-04-14 18:54:16 -0700

    Packaged tor files for Windows

--- install/build_exe.bat
+++ install/build_exe.bat
@@ -11,7 +11,7 @@ REM sign onionshare-gui.exe
 REM signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 dist\onionshare\onionshare-gui.exe
 
 REM build an installer, dist\OnionShare_Setup.exe
-REM makensis.exe install\onionshare.nsi
+makensis.exe install\onionshare.nsi
 
 REM sign OnionShare_Setup.exe
 REM signtool.exe sign /v /d "OnionShare" /a /tr http://timestamp.globalsign.com/scripts/timstamp.dll /fd sha256 dist\OnionShare_Setup.exe
