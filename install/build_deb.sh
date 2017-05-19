157d790e27a10e6f62cad7c3ae00af9d9a5d44fa
Miguel Jacq <mig@mig5.net>
2017-05-19 14:36:28 +1000

    account for the fact that stdeb's util.py renames .dev to ~dev, in version names

--- install/build_deb.sh
+++ install/build_deb.sh
@@ -13,6 +13,9 @@ python3 setup.py --command-packages=stdeb.command bdist_deb
 
 # return install instructions if onionshare builds properly
 if [[ $? -eq 0 ]]; then
+	# The build process in stdeb's util.py renames .dev to ~dev
+	# Adjust it here for the purposes of displaying the right filename
+	VERSION="${VERSION/.dev/~dev}"
 	echo ""
 	echo "To install, run:"
 	echo "sudo dpkg -i deb_dist/onionshare_$VERSION-1_all.deb"
