a50d1207c300b3a07f3cc29a1116b87c3e0f362e
Delirious Lettuce <delirious.lettuce@gmail.com>
2017-05-28 13:38:33 -0600

    Remove `constant_time_compare` to use `hmac.compare_digest`

--- onionshare/common.py
+++ onionshare/common.py
@@ -99,26 +99,6 @@ def get_version():
         return version
 
 
-def constant_time_compare(val1, val2):
-    """
-    Returns True if the two strings are equal, False otherwise.
-
-    The time taken is independent of the number of characters that match.
-
-    For the sake of simplicity, this function executes in constant time only
-    when the two strings have the same length. It short-circuits when they
-    have different lengths.
-
-    From: http://www.levigross.com/2014/02/07/constant-time-comparison-functions-in...-python-haskell-clojure-and-java/
-    """
-    if len(val1) != len(val2):
-        return False
-    result = 0
-    for x, y in zip(val1, val2):
-        result |= x ^ y
-    return result == 0
-
-
 def random_string(num_bytes, output_len=None):
     """
     Returns a random string with a specified number of bytes.
