bc5f299ecded8c4ad35cdb42c7e55c58db5b2560
Micah Lee <micah@micahflee.com>
2018-05-04 16:26:54 -0700

    Start building Uploads widget

---
+++ onionshare_gui/receive_mode/uploads.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+OnionShare | https://onionshare.org/
+
+Copyright (C) 2014-2018 Micah Lee <micah@micahflee.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from PyQt5 import QtCore, QtWidgets, QtGui
+
+from onionshare import strings
+
+class Uploads(QtWidgets.QWidget):
+    """
+    The uploads chunk of the GUI. This lists all of the active upload
+    progress bars, as well as information about each upload.
+    """
+    def __init__(self, common):
+        super(Uploads, self).__init__()
+
+        self.common = common
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addStretch()
+        self.setLayout(self.layout)
