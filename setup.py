9f542fb86496e5c9b5480849be2ea559b87fbc49
Vinicius Zavam <egypcio@googlemail.com>
2018-07-04 17:49:41 +0200

    platform.system().endswith('BSD') ?!

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ data_files=[
         (os.path.join(sys.prefix, 'share/onionshare/locale'), file_list('share/locale')),
         (os.path.join(sys.prefix, 'share/onionshare/html'), file_list('share/html')),
     ]
-if platform.system() != 'OpenBSD':
+if not platform.system().endswith('BSD'):
     data_files.append(('/usr/share/nautilus-python/extensions/', ['install/scripts/onionshare-nautilus.py']))
 
 setup(
