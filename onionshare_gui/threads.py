34e7290b06d560a85d4824e3a47af2abaee5c76b
Miguel Jacq <mig@mig5.net>
2018-09-19 15:07:04 +1000

    Start the WebThread earlier than the Onion service. This gives it a chance to generate its slug before the Onion Service finishes starting up, which can otherwise lead to a crash

--- onionshare_gui/threads.py
+++ onionshare_gui/threads.py
@@ -41,14 +41,6 @@ class OnionThread(QtCore.QThread):
     def run(self):
         self.mode.common.log('OnionThread', 'run')
 
-        try:
-            self.mode.app.start_onion_service()
-            self.success.emit()
-
-        except (TorTooOld, TorErrorInvalidSetting, TorErrorAutomatic, TorErrorSocketPort, TorErrorSocketFile, TorErrorMissingPassword, TorErrorUnreadableCookieFile, TorErrorAuthError, TorErrorProtocolError, BundledTorTimeout, OSError) as e:
-            self.error.emit(e.args[0])
-            return
-
         self.mode.app.stay_open = not self.mode.common.settings.get('close_after_first_download')
 
         # start onionshare http service in new thread
@@ -57,6 +49,14 @@ class OnionThread(QtCore.QThread):
 
         # wait for modules in thread to load, preventing a thread-related cx_Freeze crash
         time.sleep(0.2)
+
+        try:
+            self.mode.app.start_onion_service()
+            self.success.emit()
+
+        except (TorTooOld, TorErrorInvalidSetting, TorErrorAutomatic, TorErrorSocketPort, TorErrorSocketFile, TorErrorMissingPassword, TorErrorUnreadableCookieFile, TorErrorAuthError, TorErrorProtocolError, BundledTorTimeout, OSError) as e:
+            self.error.emit(e.args[0])
+            return
 
 
 class WebThread(QtCore.QThread):
