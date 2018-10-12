f06e34e3405ad644ed9d793119cbe02337759cbf
heartsucker <heartsucker@autistici.org>
2018-10-12 20:40:05 +0200

    factor generate() into own function

--- onionshare/web/share_mode.py
+++ onionshare/web/share_mode.py
@@ -183,7 +183,7 @@ class ShareModeWeb(object):
             # Prepare some variables to use inside generate() function below
             # which is outside of the request context
             shutdown_func = request.environ.get('werkzeug.server.shutdown')
-            path = request.path
+            request_path = request.path
 
             # If this is a zipped file, then serve as-is. If it's not zipped, then,
             # if the http client supports gzip compression, gzip the file first
@@ -197,84 +197,14 @@ class ShareModeWeb(object):
                 self.filesize = self.download_filesize
 
             # Tell GUI the download started
-            self.web.add_request(self.web.REQUEST_STARTED, path, {
+            self.web.add_request(self.web.REQUEST_STARTED, request_path, {
                 'id': download_id,
                 'use_gzip': use_gzip
             })
 
             basename = os.path.basename(self.download_filename)
 
-            def generate():
-                # Starting a new download
-                if not self.web.stay_open:
-                    self.download_in_progress = True
-
-                chunk_size = 102400  # 100kb
-
-                fp = open(file_to_download, 'rb')
-                self.web.done = False
-                canceled = False
-                while not self.web.done:
-                    # The user has canceled the download, so stop serving the file
-                    if not self.web.stop_q.empty():
-                        self.web.add_request(self.web.REQUEST_CANCELED, path, {
-                            'id': download_id
-                        })
-                        break
-
-                    chunk = fp.read(chunk_size)
-                    if chunk == b'':
-                        self.web.done = True
-                    else:
-                        try:
-                            yield chunk
-
-                            # tell GUI the progress
-                            downloaded_bytes = fp.tell()
-                            percent = (1.0 * downloaded_bytes / self.filesize) * 100
-
-                            # only output to stdout if running onionshare in CLI mode, or if using Linux (#203, #304)
-                            if not self.web.is_gui or self.common.platform == 'Linux' or self.common.platform == 'BSD':
-                                sys.stdout.write(
-                                    "\r{0:s}, {1:.2f}%          ".format(self.common.human_readable_filesize(downloaded_bytes), percent))
-                                sys.stdout.flush()
-
-                            self.web.add_request(self.web.REQUEST_PROGRESS, path, {
-                                'id': download_id,
-                                'bytes': downloaded_bytes
-                                })
-                            self.web.done = False
-                        except:
-                            # looks like the download was canceled
-                            self.web.done = True
-                            canceled = True
-
-                            # tell the GUI the download has canceled
-                            self.web.add_request(self.web.REQUEST_CANCELED, path, {
-                                'id': download_id
-                            })
-
-                fp.close()
-
-                if self.common.platform != 'Darwin':
-                    sys.stdout.write("\n")
-
-                # Download is finished
-                if not self.web.stay_open:
-                    self.download_in_progress = False
-
-                # Close the server, if necessary
-                if not self.web.stay_open and not canceled:
-                    print(strings._("closing_automatically"))
-                    self.web.running = False
-                    try:
-                        if shutdown_func is None:
-                            raise RuntimeError('Not running with the Werkzeug Server')
-                        shutdown_func()
-                    except:
-                        pass
-
-            r = Response(generate())
+            r = Response(self.generate(file_to_download, request_path, download_id))
             if use_gzip:
                 r.headers.set('Content-Encoding', 'gzip')
             r.headers.set('Content-Length', self.filesize)
@@ -285,6 +215,80 @@ class ShareModeWeb(object):
             if content_type is not None:
                 r.headers.set('Content-Type', content_type)
             return r
+
+    def generate(self, file_to_download, request_path, download_id):
+        # The user hasn't canceled the download
+        self.client_cancel = False
+
+        # Starting a new download
+        if not self.web.stay_open:
+            self.download_in_progress = True
+
+        chunk_size = 102400  # 100kb
+
+        fp = open(file_to_download, 'rb')
+        self.web.done = False
+        canceled = False
+        while not self.web.done:
+            # The user has canceled the download, so stop serving the file
+            if not self.web.stop_q.empty():
+                self.web.add_request(self.web.REQUEST_CANCELED, request_path, {
+                    'id': download_id
+                })
+                break
+
+            chunk = fp.read(chunk_size)
+            if chunk == b'':
+                self.web.done = True
+            else:
+                try:
+                    yield chunk
+
+                    # tell GUI the progress
+                    downloaded_bytes = fp.tell()
+                    percent = (1.0 * downloaded_bytes / self.filesize) * 100
+
+                    # only output to stdout if running onionshare in CLI mode, or if using Linux (#203, #304)
+                    if not self.web.is_gui or self.common.platform == 'Linux' or self.common.platform == 'BSD':
+                        sys.stdout.write(
+                            "\r{0:s}, {1:.2f}%          ".format(self.common.human_readable_filesize(downloaded_bytes), percent))
+                        sys.stdout.flush()
+
+                    self.web.add_request(self.web.REQUEST_PROGRESS, request_path, {
+                        'id': download_id,
+                        'bytes': downloaded_bytes
+                        })
+                    self.web.done = False
+                except:
+                    # looks like the download was canceled
+                    self.web.done = True
+                    canceled = True
+
+                    # tell the GUI the download has canceled
+                    self.web.add_request(self.web.REQUEST_CANCELED, request_path, {
+                        'id': download_id
+                    })
+
+        fp.close()
+
+        if self.common.platform != 'Darwin':
+            sys.stdout.write("\n")
+
+        # Download is finished
+        if not self.web.stay_open:
+            self.download_in_progress = False
+
+        # Close the server, if necessary
+        if not self.web.stay_open and not canceled:
+            print(strings._("closing_automatically"))
+            self.web.running = False
+            try:
+                if shutdown_func is None:
+                    raise RuntimeError('Not running with the Werkzeug Server')
+                shutdown_func()
+            except:
+                pass
+
 
     def set_file_info(self, filenames, processed_size_callback=None):
         """
