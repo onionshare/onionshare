042f89d31c853af30c1803eead333d06995ff9a4
Miguel Jacq <mig@mig5.net>
2018-10-17 14:33:31 +1100

    Add a test for making sure quitting during a share prompts before shutting down share

---
+++ tests/local_onionshare_quitting_during_share_prompts_warning_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import unittest
+from PyQt5 import QtCore, QtTest
+
+from .GuiShareTest import GuiShareTest
+
+class LocalQuittingDuringSharePromptsWarningTest(unittest.TestCase, GuiShareTest):
+    @classmethod
+    def setUpClass(cls):
+        test_settings = {
+            "close_after_first_download": False
+        }
+        cls.gui = GuiShareTest.set_up(test_settings, 'LocalQuittingDuringSharePromptsWarningTest')
+
+    #@classmethod
+    #def tearDownClass(cls):
+    #    TorGuiShareTest.tear_down()
+
+    def test_gui(self):
+        self.run_all_common_setup_tests()
+        self.run_all_share_mode_tests(False, True)
+        # Prepare our auto-accept of prompt
+        QtCore.QTimer.singleShot(5000, self.accept_dialog)
+        # Try to close the app
+        self.gui.close()
+        # Server should still be running (we've been prompted first)
+        self.server_is_started(self.gui.share_mode, 0)
+        self.web_server_is_running()
+
+if __name__ == "__main__":
+    unittest.main()
