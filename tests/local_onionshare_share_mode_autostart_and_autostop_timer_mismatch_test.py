3845c5b3cf3724f484ead2f2022394a70d282986
Micah Lee <micah@micahflee.com>
2019-04-18 20:38:44 -0700

    Skip GUI and tor tests if pytest is older than 2.9, to allow tests to run in old ubuntu

--- tests/local_onionshare_share_mode_autostart_and_autostop_timer_mismatch_test.py
+++ tests/local_onionshare_share_mode_autostart_and_autostop_timer_mismatch_test.py
@@ -19,6 +19,7 @@ class LocalShareModeAutoStartTimerTest(unittest.TestCase, GuiShareTest):
         GuiShareTest.tear_down()
 
     @pytest.mark.gui
+    @pytest.mark.skipif(pytest.__version__ < '2.9', reason="requires newer pytest")
     def test_gui(self):
         self.run_all_common_setup_tests()
         self.run_all_share_mode_autostop_autostart_mismatch_tests(False)
--- tests/local_onionshare_share_mode_download_test.py
+++ tests/local_onionshare_share_mode_download_test.py
@@ -16,6 +16,7 @@ class LocalShareModeTest(unittest.TestCase, GuiShareTest):
         GuiShareTest.tear_down()
 
     @pytest.mark.gui
+    @pytest.mark.skipif(pytest.__version__ < '2.9', reason="requires newer pytest")
     def test_gui(self):
         self.run_all_common_setup_tests()
         self.run_all_share_mode_tests(False, False)
