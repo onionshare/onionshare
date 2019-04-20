a7bbe818bff45a3248180aefa0b0a2a39e158f14
Miguel Jacq <mig@mig5.net>
2019-04-20 10:31:34 +1000

    Unlocalize CLI strings

--- install/check_lacked_trans.py
+++ install/check_lacked_trans.py
@@ -56,8 +56,9 @@ def main():
 
     src = files_in(dir, 'onionshare') + \
           files_in(dir, 'onionshare_gui') + \
-          files_in(dir, 'onionshare_gui/share_mode') + \
-          files_in(dir, 'onionshare_gui/receive_mode') + \
+          files_in(dir, 'onionshare_gui/mode') + \
+          files_in(dir, 'onionshare_gui/mode/share_mode') + \
+          files_in(dir, 'onionshare_gui/mode/receive_mode') + \
           files_in(dir, 'install/scripts') + \
           files_in(dir, 'tests')
     pysrc = [p for p in src if p.endswith('.py')]
