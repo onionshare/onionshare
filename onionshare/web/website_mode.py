9805919fc7ec395da19979b20226c1de037c7969
hiro <hiro@torproject.org>
2019-06-13 12:41:12 +0200

    Move directory_listing function

--- onionshare/web/website_mode.py
+++ onionshare/web/website_mode.py
@@ -97,38 +97,7 @@ class WebsiteModeWeb(BaseModeWeb):
                     # If the path isn't found, throw a 404
                     return self.web.error404()
 
-    def directory_listing(self, path, filenames, filesystem_path=None):
-        # If filesystem_path is None, this is the root directory listing
-        files = []
-        dirs = []
-
-        for filename in filenames:
-            if filesystem_path:
-                this_filesystem_path = os.path.join(filesystem_path, filename)
-            else:
-                this_filesystem_path = self.files[filename]
-
-            is_dir = os.path.isdir(this_filesystem_path)
-
-            if is_dir:
-                dirs.append({
-                    'basename': filename
-                })
-            else:
-                size = os.path.getsize(this_filesystem_path)
-                size_human = self.common.human_readable_filesize(size)
-                files.append({
-                    'basename': filename,
-                    'size_human': size_human
-                })
-
-        r = make_response(render_template('listing.html',
-            path=path,
-            files=files,
-            dirs=dirs,
-            static_url_path=self.web.static_url_path))
-        return self.web.add_security_headers(r)
-
+    
     def build_file_list(self, filenames):
         """
         Build a data structure that describes the list of files that make up
