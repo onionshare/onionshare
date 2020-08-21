8abf2f946adda9e1af9a11846aa21deec0907c34
Micah Lee <micah@micahflee.com>
2020-08-21 18:34:14 -0400

    Windows batch script for running tests

---
+++ tests/run.bat
@@ -0,0 +1,9 @@
+pytest -vvv tests\test_cli.py
+pytest -vvv tests\test_cli_common.py
+pytest -vvv tests\test_cli_settings.py
+pytest -vvv tests\test_cli_strings.py
+pytest -vvv tests\test_cli_web.py
+pytest -vvv --rungui tests\test_gui_tabs.py
+pytest -vvv --rungui tests\test_gui_share.py
+pytest -vvv --rungui tests\test_gui_receive.py
+pytest -vvv --rungui tests\test_gui_website.py
