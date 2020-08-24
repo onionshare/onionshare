50ae9945359fc88cae7c428f5f7f52b77ac47aa3
Micah Lee <micah@micahflee.com>
2020-08-24 19:56:34 -0400

    Add some OnionShare styling to the theme and write some more docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ html_theme = "sphinx_rtd_theme"
 html_logo = "_static/logo.svg"
 html_theme_options = {}
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 # sphinx-intl
 locale_dirs = ["locale/"]
