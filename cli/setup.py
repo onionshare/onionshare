19b73ae5717b2c994f3656d52cfcb01fd2142e79
Micah Lee <micah@micahflee.com>
2020-11-08 12:07:17 -0800

    Add incomplete flatpak manifest, update release docs, re-add source package script

--- cli/setup.py
+++ cli/setup.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-# snap packaging uses setup.py
+# snap and flatpak packaging uses setup.py
 # PyPi publishing, developing, and testing uses poetry
 
 import os
