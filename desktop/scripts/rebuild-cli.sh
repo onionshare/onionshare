7bcfe6cad11036feeb52b199a9f0736ff35e21d6
Micah Lee <micah@micahflee.com>
2020-11-29 10:42:04 -0800

    Add rebuild-cli.sh script

---
+++ desktop/scripts/rebuild-cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Build the CLI python wheel and copy it to the desktop folder
+
+SCRIPTS_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
+cd $SCRIPTS_DIR
+cd ../../cli
+poetry build
+cp dist/*.whl ../desktop
