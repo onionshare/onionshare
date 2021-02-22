4915c80eed542b1cc8835ddec93b15695e20ff4e
Micah Lee <micah@micahflee.com>
2021-02-21 17:13:15 -0800

    Tiny changes during the 2.3 release

--- desktop/scripts/get-tor-windows.py
+++ desktop/scripts/get-tor-windows.py
@@ -35,7 +35,7 @@ import requests
 
 def main():
     exe_url = "https://archive.torproject.org/tor-package-archive/torbrowser/10.0.10/torbrowser-install-10.0.10_en-US.exe"
-    exe_filename = "torbrowser-install-10.0.2_en-US.exe"
+    exe_filename = "torbrowser-install-10.0.10_en-US.exe"
     expected_exe_sha256 = (
         "6cbd14a7232e4ae7f2718d9b7f377e1a7bb96506da21f1ac6f689a22fc5e53fe"
     )
