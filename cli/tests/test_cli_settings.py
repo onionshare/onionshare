70dce471cde0ca0783c343fe0c1fe17b97622d7f
Micah Lee <micah@micahflee.com>
2021-10-17 14:26:56 -0700

    Save/load moat bridges to/from settings

--- cli/tests/test_cli_settings.py
+++ cli/tests/test_cli_settings.py
@@ -33,6 +33,8 @@ class TestSettings:
             "tor_bridges_use_obfs4": False,
             "tor_bridges_use_meek_lite_azure": False,
             "tor_bridges_use_snowflake": False,
+            "tor_bridges_use_moat": False,
+            "tor_bridges_use_moat_bridges": "",
             "tor_bridges_use_custom_bridges": "",
             "persistent_tabs": [],
             "theme": 0,
