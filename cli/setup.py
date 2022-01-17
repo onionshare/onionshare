a5f97b6c881b6d1053836370c6df5ff8497214ac
Micah Lee <micah@micahflee.com>
2022-01-16 16:06:29 -0800

    Version bump to 2.4.1

--- cli/setup.py
+++ cli/setup.py
@@ -18,9 +18,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import os
 import setuptools
 
-version = "2.4"
+with open(os.path.join("onionshare_cli", "resources", "version.txt")) as f:
+    version = f.read().strip()
 
 setuptools.setup(
     name="onionshare-cli",
