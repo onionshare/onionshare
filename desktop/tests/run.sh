06eb12cf8f1494cd0c81f060496d33b3b843dd77
Micah Lee <micah@micahflee.com>
2022-03-06 14:17:13 -0800

    Try running tests a different way

--- desktop/tests/run.sh
+++ desktop/tests/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pytest -v tests/test_gui_tabs.py && sleep 5 && \
-pytest -v tests/test_gui_share.py && sleep 5 && \
-pytest -v tests/test_gui_receive.py && sleep 5 && \
-pytest -v tests/test_gui_website.py && sleep 5 && \
+pytest -v tests/test_gui_tabs.py && \
+pytest -v tests/test_gui_share.py && \
+pytest -v tests/test_gui_receive.py && \
+pytest -v tests/test_gui_website.py && \
 pytest -v tests/test_gui_chat.py
