887b9672788a59750ef895ff1e7d835249231664
Miguel Jacq <mig@mig5.net>
2025-02-21 17:36:48 +1100

    Replace zh_Hans with zh_CN for docs as suggested by Weblate team

--- docs/build.sh
+++ docs/build.sh
@@ -3,7 +3,7 @@
 VERSION=$(cat ../cli/onionshare_cli/resources/version.txt)
 
 # Supported locales
-LOCALES="en sq bg zh_Hans de el ga ja pl ru es tr uk"
+LOCALES="en sq bg zh_CN de el ga ja pl ru es tr uk"
 
 # Generate English .po files
 make gettext
