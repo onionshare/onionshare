a5165d1e09b6cf795658a35e73dcbeef7d7e1ccd
Kushal Das <mail@kushaldas.in>
2025-12-10 16:12:41 +0100

    fix: importing importlib.metadata

--- cli/onionshare_cli/web/web.py
+++ cli/onionshare_cli/web/web.py
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import importlib
+import importlib.metadata
 import logging
 import mimetypes
 import os
